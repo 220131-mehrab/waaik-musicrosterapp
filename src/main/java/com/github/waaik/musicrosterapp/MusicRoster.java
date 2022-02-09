83596ed2f80b459bd528d5b56387a9c75dc4a1e0
Waaik Perrin <keythageniuz@Waaiks-MacBook-Pro-2.local>
2022-02-09 14:29:51 -0500

    Restarting Coding for Program in IDE

---
+++ src/main/java/com/github/waaik/musicrosterapp/MusicRoster.java
@@ -0,0 +1,8 @@
+package com.github.waaik.musicrosterapp;
+  
+public class MusicRoster {
+        public static void main(String[] args){
+                System.out.println("Welcome to the Music Roster App!");
+        }
+}
+
